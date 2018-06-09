01bae6fe79d1f3c8742a8ce1f1c8a03f95717eca
[Volodymyr Bondarenko] <[RedefinePL@gmail.com]>
2018-06-09 16:36:03 -0400

    sile face added

--- ClassToGit.java
+++ ClassToGit.java
@@ -2,6 +2,39 @@ public class ClassToGit{
      public static void main(String[] args){
      
      System.out.println("hello world");
+System.out.println();
+System.out.println("                     8888888888888                     ");
+System.out.println("                  8888888888888888888                  ");
+System.out.println("               8888888888888888888888888               ");
+System.out.println("             88888888888888888888888888888             ");
+System.out.println("           888888888888888888888888888888888           ");
+System.out.println("         8888888888888888888888888888888888888         ");
+System.out.println("        888888888888888888888888888888888888888        ");
+System.out.println("       88888888888888888888888888888888888888888       ");
+System.out.println("      8888888888     8888888888888888888888888888      ");
+System.out.println("     8888888888       8888888888888888888888888888     ");
+System.out.println("     8888888888       8888888888888888888888888888     ");
+System.out.println("    88888888888       88888888888888888888888888888    ");
+System.out.println("    88888888888       8888888888        88888888888    ");
+System.out.println("   8888888888888     8888888888888888888888888888888   ");
+System.out.println("   8888888888888888888888888888888888888888888888888   ");
+System.out.println("   8888888888888888888888888888888888888888888888888   ");
+System.out.println("   8888888888888888888888888888888888888888888888888   ");
+System.out.println("   8888888888888888888888888888888888888888888888888   ");
+System.out.println("    88888888888888888888888888888888888888888888888    ");
+System.out.println("    88888888888888888888888888888888888888888888888    ");
+System.out.println("     888888888888888888888888888888888888888888888     ");
+System.out.println("     888888  88888888888888888888888888888  888888     ");
+System.out.println("      888888  888888888888888888888888888  888888      ");
+System.out.println("       888888   88888888888888888888888   888888       ");
+System.out.println("        8888888    88888888888888888    8888888        ");
+System.out.println("          8888888      888888888      88888888         ");
+System.out.println("            88888888               888888888           ");
+System.out.println("             8888888888         8888888888             ");
+System.out.println("               8888888888888888888888888               ");
+System.out.println("                  8888888888888888888                  ");
+System.out.println("                      88888888888                      ");
+System.out.println();
 
 
      }
